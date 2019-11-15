c9bfbb8cafc6c72d4e4ed1cb7cd4c5832d697bad
Pierre <pm4rtin@gmail.com>
2019-11-15 11:14:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const video = document.getElementById("video");
+
+function startVideo() {
+  navigator.getUserMedia(
+    {
+      video: {}
+    },
+    stream => (video.srcObject = stream),
+    err => console.log(err)
+  );
+}
+
+startVideo();
