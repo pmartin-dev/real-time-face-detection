7258b0b4aa3d23b9fb17494fe6f3780e8f900d77
Pierre <pm4rtin@gmail.com>
2019-11-15 12:35:02 +0100

    real time face recognition implementation

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const video = document.getElementById("video");
 
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models")
+]).then(startVideo);
+
 function startVideo() {
   navigator.getUserMedia(
     {
@@ -10,4 +17,31 @@ function startVideo() {
   );
 }
 
-startVideo();
+// video.addEventListener("play", () => {
+//   setInterval(async () => {
+//     const detections = await faceapi
+//       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+//       .withFaceLandmarks()
+//       .withFaceExpressions();
+//     console.log(detections);
+//   }, 1000);
+// });
+
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    console.log(detections);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 100);
+});
